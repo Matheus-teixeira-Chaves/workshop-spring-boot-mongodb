81b4c243779bcfacb1b6f52bb1b6ef3d5bb5e60f
Matheus Frazão <73357559+Matheus-teixeira-Chaves@users.noreply.github.com>
2022-11-04 21:03:53 -0300

    Obtendo um usuario por id

--- src/main/java/com/matheusfrazao/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusfrazao/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.matheusfrazao.workshopmongo.domain.User;
 import com.matheusfrazao.workshopmongo.repository.UserRepository;
+import com.matheusfrazao.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 	
 	public List <User> findAll () {
 		return repo.findAll();
+	}
+	
+	public User findById (String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
