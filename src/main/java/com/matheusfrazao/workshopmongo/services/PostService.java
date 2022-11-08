acf0c86d7f1f0ea7692f9c8a1802b75f8552d2aa
Matheus Frazão <73357559+Matheus-teixeira-Chaves@users.noreply.github.com>
2022-11-08 19:23:39 -0300

    Obtendo um post por id

---
+++ src/main/java/com/matheusfrazao/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.matheusfrazao.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusfrazao.workshopmongo.domain.Post;
+import com.matheusfrazao.workshopmongo.repository.PostRepository;
+import com.matheusfrazao.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById (String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+}
